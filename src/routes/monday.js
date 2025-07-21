92808a16b56641908156191042e44c396e534e77
Rodney Buljubasic <rodney@bootepolch.local>
2025-07-21 14:00:16 +0200

    Remove unused assignperson recipe and route

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -4,12 +4,10 @@ const mondayController = require('../controllers/monday-controller');
 
 
 
-const assignPersonController = require('../recipes/assignperson/controller');
 const transformNewController = require('../recipes/transformnew/controller');
 const updateController = require('../recipes/update/controller');
 const xyzController = require('../recipes/xyz/controller');
 
-router.post('/monday/assignperson', authenticationMiddleware, assignPersonController.handler);
 router.post('/monday/transformnew', authenticationMiddleware, transformNewController.handler);
 router.post('/monday/update', authenticationMiddleware, updateController.handler);
 router.post('/monday/xyz', authenticationMiddleware, xyzController.handler);
