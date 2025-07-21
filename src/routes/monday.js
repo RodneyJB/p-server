d2734aef7d3ac5636c923eeb7f451d165a44e259
Rodney Buljubasic <rodney@bootepolch.local>
2025-07-21 14:06:04 +0200

    Rename TransformNew to transform for clean routing

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -4,11 +4,11 @@ const mondayController = require('../controllers/monday-controller');
 
 
 
-const transformNewController = require('../recipes/transformnew/controller');
+const transformNewController = require('../recipes/transform/controller');
 const updateController = require('../recipes/update/controller');
 const xyzController = require('../recipes/xyz/controller');
 
-router.post('/monday/transformnew', authenticationMiddleware, transformNewController.handler);
+router.post('/monday/transform', authenticationMiddleware, transformNewController.handler);
 router.post('/monday/update', authenticationMiddleware, updateController.handler);
 router.post('/monday/xyz', authenticationMiddleware, xyzController.handler);
 
